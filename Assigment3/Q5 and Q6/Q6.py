1d988487e9c56d5ccc9a6b973a1ba366af4cf35f
ZCZDinn <christian.dinn12@ed.cna.nl.ca>
2024-04-14 23:36:51 -0230

    Q6 progress

--- Assigment3/Q5 and Q6/Q6.py
+++ Assigment3/Q5 and Q6/Q6.py
@@ -2,13 +2,17 @@ import Question5 as q5
 import tkinter as tk
 from tkinter import ttk,messagebox
 
+
 padding = '5 5 5 5'
 EditSalesData = tk.Tk()
 EditSalesData.title('Edit Sales Data')
 EditSalesData.geometry('400x300')
 
+def clicked_button1():
+    DailySales = q5.DailySales()
+    q5.
 frame = ttk.Frame(EditSalesData, padding=padding)
-frame.pack(fill='both',expand=True)
+frame.pack(fill='both', expand=True)
 
 instruction = ttk.Label(frame, text='Enter date and region to get sales amount.')
 instruction.grid(row=0, column=0, columnspan=4, sticky=tk.N)
@@ -41,5 +45,8 @@ id_label.grid(row=4, column=0, sticky=tk.E)
 id = tk.IntVar(value=0)
 idEntry = tk.Entry(frame,width=25, textvariable=id, state="readonly")
 idEntry.grid(row =4, column=1)
+
+button1 = ttk.Button(frame, text="Get Amount", command=clicked_button1)
+button1.grid(column=0, row=2)
 
 EditSalesData.mainloop()
