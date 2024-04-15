d4e551dc364c23102a8e50da61f4c8491894310c
ZCZDinn <christian.dinn12@ed.cna.nl.ca>
2024-04-14 23:30:59 -0230

    Q6 progress

--- Assigment3/Q5 and Q6/Q6.py
+++ Assigment3/Q5 and Q6/Q6.py
@@ -7,39 +7,39 @@ EditSalesData = tk.Tk()
 EditSalesData.title('Edit Sales Data')
 EditSalesData.geometry('400x300')
 
-window = ttk.Frame(EditSalesData, padding=padding)
-window.pack(fill='both',expand=True)
+frame = ttk.Frame(EditSalesData, padding=padding)
+frame.pack(fill='both',expand=True)
 
-instruction = ttk.Label(window, text='Enter date and region to get sales amount.')
-instruction.grid(row=0)
+instruction = ttk.Label(frame, text='Enter date and region to get sales amount.')
+instruction.grid(row=0, column=0, columnspan=4, sticky=tk.N)
 
-date_label = ttk.Label(window, width = 5, text='Date: ',padding=padding)
+date_label = ttk.Label(frame, width = 10, text='Date: ',padding=padding,)
 date_label.grid(row =1, column=0, sticky=tk.W)
 
 
 date = tk.StringVar(value='')
-dateEntry = tk.Entry(window,width=25, textvariable=date)
-dateEntry.grid(row =1, column=1, sticky=tk.W)
+dateEntry = tk.Entry(frame,width=25, textvariable=date)
+dateEntry.grid(row =1, column=1, columnspan=2, sticky=tk.W)
 
-region_label = ttk.Label(window, width = 5, text='Region: ',padding=padding)
+region_label = ttk.Label(frame, width = 10, text='Region: ',padding=padding)
 region_label.grid(row =2, column=0, sticky=tk.W)
 
 region = tk.StringVar(value='')
-regionEntry = tk.Entry(window,width=25, textvariable=region)
-regionEntry.grid(row =2, column=1, sticky=tk.W)
+regionEntry = tk.Entry(frame,width=25, textvariable=region)
+regionEntry.grid(row =2, column=1, columnspan=2, sticky=tk.W)
 
-amount_label = ttk.Label(window, width = 5, text='Amount: ',padding=padding)
+amount_label = ttk.Label(frame, width = 10, text='Amount: ',padding=padding)
 amount_label.grid(row =3, column=0, sticky=tk.W)
 
 amount = tk.IntVar(value=0)
-amountEntry = tk.Entry(window,width=25, textvariable=amount)
-amountEntry.grid(row =3, column=1, sticky=tk.W)
+amountEntry = tk.Entry(frame,width=25, textvariable=amount)
+amountEntry.grid(row =3, column=1)
 
-id_label = ttk.Label(window, width = 5, text='ID: ',padding=padding)
-id_label.grid(row =4, column=0, sticky=tk.W)
+id_label = ttk.Label(frame, width=10, text='ID: ',padding=padding)
+id_label.grid(row=4, column=0, sticky=tk.E)
 
 id = tk.IntVar(value=0)
-idEntry = tk.Entry(window,width=25, textvariable=id)
-idEntry.grid(row =4, column=1, sticky=tk.W)
+idEntry = tk.Entry(frame,width=25, textvariable=id, state="readonly")
+idEntry.grid(row =4, column=1)
 
 EditSalesData.mainloop()
